f09c34dc6f018fb1987f991f41e195fb41be0fe6
JinHwan-Park0428 <zkzlaptb6@gmail.com>
2021-04-22 10:59:20 +0900

    SQL Injection 가능한 views 코드 업로드

--- DBConnect/views.py
+++ DBConnect/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets
 from rest_framework import permissions
 from DBConnect.models import Djangotest
 from DBConnect.serializers import DjangotestSerializer
-# from django.db import connection
+from django.db import connection
 
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -15,15 +15,36 @@ class DjangotestViewSet(viewsets.ReadOnlyModelViewSet):
 
     permission_classes = [permissions.IsAuthenticated]
 
-    # /my_topic_MyProjectWeather/search?q=test5
+    # sql 인젝션 방어용 코드
+    # @action(detail=False, methods=['GET'])
+    # def search(self, request):
+    #     q = request.query_params.get('q', None)
+    #     print("q값 출력:", type(q))
+    #
+    #     qs = self.get_queryset().filter(userkey=q)
+    #     serializer = self.get_serializer(qs, many=True)
+    #     # print(serializer.data)
+    #
+    #     return Response(serializer.data)
+
+    #sql 인젝션 되는 코드
     @action(detail=False, methods=['GET'])
     def search(self, request):
-        q = request.query_params.get('q', None)
+        # books = Book.objects.all()
+        try:
+            cursor = connection.cursor()
+            q = request.query_params.get('q', None)
+            strSql = "SELECT userid, userpwd FROM Djangotest where userkey=" +q
+            result = cursor.execute(strSql)
+            datas = cursor.fetchall()
+            print(datas)
 
-        qs = self.get_queryset().filter(userkey=q)
-        serializer = self.get_serializer(qs, many=True)
-        print(serializer.data)
+            connection.commit()
+            connection.close()
 
-        return Response(serializer.data)
+        except:
+            connection.rollback()
+            print("Failed selecting in BookListView")
 
+        return Response(datas)
 
