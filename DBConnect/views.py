696c5e77d5349d89a1d7a8f187af967549660b90
JinHwan-Park0428 <zkzlaptb6@gmail.com>
2021-05-07 15:21:49 +0900

    결제 결과 출력 수정

--- DBConnect/views.py
+++ DBConnect/views.py
@@ -2794,6 +2794,62 @@ class SkdevsecOrderuserViewSet(viewsets.ReadOnlyModelViewSet):
         else:
             return Response(new_data)
 
+    # sql 인젝션 되는 코드
+    # 결제 결과 내역 출력
+    @action(detail=False, methods=['POST'])
+    def user_paid_input(self, request):
+        # 데이터 저장을 위한 리스트 선언
+        new_data = list()
+        try:
+            # DB 접근할 cursor
+            cursor = connection.cursor()
+
+            # POST 메소드로 날라온 Request의 데이터 각각 추출
+            oid = request.data['oid']
+
+            # SQL 쿼리문 작성
+            strsql = "SELECT * FROM skdevsec_orderuser where oid='" + oid + "'"
+
+            # DB에 명령문 전송
+            cursor.execute(strsql)
+            datas = cursor.fetchone()
+
+            # 데이터가 있으면
+            if datas is not None:
+                # 데이터 수만큼 반복
+                while datas:
+                    new_data_in = dict()
+                    new_data_in['oid'] = datas[0]
+                    new_data_in['uid'] = datas[1]
+                    new_data_in['oname'] = datas[2]
+                    new_data_in['ophone'] = datas[3]
+                    new_data_in['oaddress'] = datas[4]
+                    new_data_in['order_date'] = datas[5]
+                    new_data_in['oprice'] = datas[6]
+                    new_data.append(new_data_in)
+                    datas = cursor.fetchone()
+            # 데이터가 없으면
+            else:
+                # DB와 접속 종료
+                connection.commit()
+                connection.close()
+                # 프론트엔드에 0 전송
+                return Response(0)
+
+            # DB와 접속 종료
+            connection.commit()
+            connection.close()
+
+        # 에러가 발생했을 경우 백엔드에 에러 내용 출력 및 프론트엔드에 0 전송
+        except Exception as e:
+            connection.rollback()
+            print(f"user_paid_input 에러: {e}")
+            return Response(0)
+
+        # 성공 했을 시, 프론트엔드에 데이터 전송
+        else:
+            return Response(new_data)
+
 
 # 결제 내역 테이블
 class SkdevsecOrderproductViewSet(viewsets.ReadOnlyModelViewSet):
