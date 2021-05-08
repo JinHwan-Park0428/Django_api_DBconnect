307408abf993c7c7fbf34a44104a9b95e2b7e445
JinHwan-Park0428 <zkzlaptb6@gmail.com>
2021-05-08 10:32:59 +0900

    일부 기능 추가

--- DBConnect/views.py
+++ DBConnect/views.py
@@ -117,25 +117,30 @@ class SkdevsecUserViewSet(viewsets.ReadOnlyModelViewSet):
         if request.data['authority'] == '1' and request.data['unickname'] == 'admin':
             # 데이터 저장을 위한 리스트 선언
             new_data = list()
+            count = 0
             try:
                 # POST 메소드로 날라온 Request의 데이터 각각 추출
                 ucode = request.data['ucode']
                 usearch = request.data['usearch']
+                upage = request.data['upage']
+                upage = int(upage)
                 ucode = int(ucode)
 
                 # DB 접근할 cursor
                 cursor = connection.cursor()
 
                 # SQL 쿼리문 작성
-                # 0 : 이름 / 1 : 닉네임 / 2 : 아이디 / 3 : 이메일
+                # 0: 전체 검색 / 1 : 이름 / 2 : 닉네임 / 3 : 아이디 / 4 : 이메일
                 if ucode == 0:
-                    strsql = "SELECT * FROM skdevsec_user where uname LIKE '%" + usearch + "%'"
+                    strsql = "SELECT * FROM skdevsec_user where (uname LIKE '%" + usearch + "%' OR unickname LIKE '%" + usearch + "%' OR uid LIKE '%" + usearch + "%' OR umail LIKE '%" + usearch + "%') order by ucreate_date desc limit " + str(upage * 10 - 10) + ", 10"
                 elif ucode == 1:
-                    strsql = "SELECT * FROM skdevsec_user where unickname LIKE '%" + usearch + "%'"
+                    strsql = "SELECT * FROM skdevsec_user where uname LIKE '%" + usearch + "%' order by ucreate_date desc limit " + str(upage * 10 - 10) + ", 10"
                 elif ucode == 2:
-                    strsql = "SELECT * FROM skdevsec_user where uid LIKE '%" + usearch + "%'"
+                    strsql = "SELECT * FROM skdevsec_user where unickname LIKE '%" + usearch + "%' order by ucreate_date desc limit " + str(upage * 10 - 10) + ", 10"
                 elif ucode == 3:
-                    strsql = "SELECT * FROM skdevsec_user where uemail LIKE '%" + usearch + "%'"
+                    strsql = "SELECT * FROM skdevsec_user where uid LIKE '%" + usearch + "%' order by ucreate_date desc limit " + str(upage * 10 - 10) + ", 10"
+                elif ucode == 4:
+                    strsql = "SELECT * FROM skdevsec_user where umail LIKE '%" + usearch + "%' order by ucreate_date desc limit " + str(upage * 10 - 10) + ", 10"
                 else:
                     return Response("코드 값 잘못 보냄!!")
 
@@ -147,6 +152,7 @@ class SkdevsecUserViewSet(viewsets.ReadOnlyModelViewSet):
                 if datas is not None:
                     # 데이터 만큼 반복
                     while datas:
+                        count += 1
                         new_data_in = dict()
                         new_data_in['uid'] = datas[0]
                         # new_data_in['upwd'] = datas[1]
@@ -165,6 +171,8 @@ class SkdevsecUserViewSet(viewsets.ReadOnlyModelViewSet):
                     connection.close()
                     # 프론트엔드에 0 전송
                     return Response(0)
+
+                new_data.append({"user_count": count})
 
                 # DB와 접속 종료
                 connection.commit()
@@ -1074,6 +1082,71 @@ class SkdevsecBoardViewSet(viewsets.ReadOnlyModelViewSet):
         else:
             return Response(new_data)
 
+    # sql 인젝션 되는 코드
+    # 게시물 검색
+    @action(detail=False, methods=['POST'])
+    def my_board_search(self, request):
+        # 데이터 저장을 위한 리스트 선언
+        new_data = list()
+        count = 0
+        try:
+            # DB 접근할 cursor
+            cursor = connection.cursor()
+
+            # POST 메소드로 날라온 Request의 데이터 각각 추출
+            unickname = request.data['unickname']
+            bsearch = request.data['bsearch']
+            bpage = request.data['bpage']
+            bpage = int(bpage)
+
+            # SQL 쿼리문 작성
+            strsql = "SELECT bid, btitle, bfile, bview, bcomment, unickname, bcreate_date FROM skdevsec_board WHERE (btitle LIKE '%" + bsearch + "%' OR btext LIKE '%" + bsearch + "%') AND unickname='" + unickname + "' ORDER BY bid DESC limit " + str(
+                bpage * 10 - 10) + ", 10"
+
+            # DB에 명령문 전송
+            cursor.execute(strsql)
+            datas = cursor.fetchone()
+
+            # 데이터가 있으면
+            if datas is not None:
+                # 데이터만큼 반복
+                while datas:
+                    count += 1
+                    new_data_in = dict()
+                    new_data_in['bid'] = datas[0]
+                    new_data_in['btitle'] = datas[1]
+                    new_data_in['bfile'] = datas[2]
+                    new_data_in['bview'] = datas[3]
+                    new_data_in['bcomment'] = datas[4]
+                    new_data_in['unickname'] = datas[5]
+                    new_data_in['bcreate_date'] = datas[6]
+                    new_data_in['b_lock'] = 0
+                    new_data.append(new_data_in)
+                    datas = cursor.fetchone()
+            # 데이터가 없으면
+            else:
+                # DB와 접속 종료
+                connection.commit()
+                connection.close()
+                # 프론트엔드에 0 전송
+                return Response(0)
+
+            new_data.append({"board_count": count})
+
+            # DB와 접속 종료
+            connection.commit()
+            connection.close()
+
+        # 에러가 발생했을 경우 백엔드에 에러 내용 출력 및 프론트엔드에 0 전송
+        except Exception as e:
+            connection.rollback()
+            print(f"board_search 에러: {e}")
+            return Response(0)
+
+        # 성공 했을 시, 프론트엔드에 데이터 전송
+        else:
+            return Response(new_data)
+
 
 # 댓글 관련 테이블
 class SkdevsecCommentViewSet(viewsets.ReadOnlyModelViewSet):
@@ -1649,6 +1722,7 @@ class SkdevsecProductViewSet(viewsets.ReadOnlyModelViewSet):
     # 상품 등록
     @action(detail=False, methods=['POST'])
     def product_upload(self, request):
+
         # 데이터 저장을 위한 딕셔너리 설정
         new_data = dict()
         try:
@@ -2119,6 +2193,81 @@ class SkdevsecProductViewSet(viewsets.ReadOnlyModelViewSet):
         except Exception as e:
             connection.rollback()
             print(f"best_item_list 에러: {e}")
+            return Response(0)
+
+        # 성공 했을 시, 프론트엔드에 데이터 전송
+        else:
+            return Response(new_data)
+
+    # sql 인젝션 되는 코드
+    # 관리자 상품 검색
+    @action(detail=False, methods=['POST'])
+    def admin_product_search(self, request):
+        # 데이터 저장을 위한 리스트 선언
+        new_data = list()
+        count = 0
+        try:
+            # DB 접근할 cursor
+            cursor = connection.cursor()
+
+            # POST 메소드로 날라온 Request의 데이터 각각 추출
+            psearch = request.data['psearch']
+            ppage = request.data['ppage']
+            pcode = request.data['pcode']
+            pcode = int(pcode)
+            ppage = int(ppage)
+
+            # SQL 쿼리문 작성
+            # 0 : 전체 검색 / 1 : 상품 명 검색 / 2 : 카테고리 검색
+            if pcode == 0:
+                strsql = "SELECT * FROM skdevsec_product WHERE (pname LIKE '%" + psearch + "%' OR pcate LIKE '%" + psearch + "%') ORDER BY pid DESC limit " + str(ppage * 8 - 8) + ", 8"
+            elif pcode == 1:
+                strsql = "SELECT * FROM skdevsec_product WHERE pname LIKE '%" + psearch + "%' ORDER BY pid DESC limit " + str(ppage * 8 - 8) + ", 8"
+            elif pcode == 2:
+                strsql = "SELECT * FROM skdevsec_product WHERE pcate LIKE '%" + psearch + "%') ORDER BY pid DESC limit " + str(ppage * 8 - 8) + ", 8"
+            else:
+                return Response(0)
+
+            # DB에 명령문 전송
+            cursor.execute(strsql)
+            datas = cursor.fetchone()
+
+            # 데이터가 있으면
+            if datas is not None:
+                # 데이터만큼 반복
+                while datas:
+                    count += 1
+                    new_data_in = dict()
+                    new_data_in['pid'] = datas[0]
+                    new_data_in['pname'] = datas[1]
+                    new_data_in['pcate'] = datas[2]
+                    new_data_in['pimage'] = datas[3]
+                    new_data_in['ptext'] = datas[4]
+                    new_data_in['pprice'] = datas[5]
+                    new_data_in['pcreate_date'] = datas[6]
+                    new_data_in['preview'] = datas[6]
+                    new_data_in['preview_avg'] = datas[6]
+                    new_data_in['pcount'] = datas[6]
+                    new_data.append(new_data_in)
+                    datas = cursor.fetchone()
+            # 데이터가 없으면
+            else:
+                # DB와 접속 종료
+                connection.commit()
+                connection.close()
+                # 프론트엔드에 0 전송
+                return Response(0)
+
+            new_data.append({"product_count": count})
+
+            # DB와 접속 종료
+            connection.commit()
+            connection.close()
+
+        # 에러가 발생했을 경우 백엔드에 에러 내용 출력 및 프론트엔드에 0 전송
+        except Exception as e:
+            connection.rollback()
+            print(f"admin_product_search 에러: {e}")
             return Response(0)
 
         # 성공 했을 시, 프론트엔드에 데이터 전송
