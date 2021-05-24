3a4f0586cadc5ea95bb61df36f9a218dd80ba349
JinHwan-Park0428 <zkzlaptb6@gmail.com>
2021-05-24 16:50:30 +0900

    skdevsec_reviewview 함수 SQL Injection 대비

--- DBConnect/skdevsec_reviewview.py
+++ DBConnect/skdevsec_reviewview.py
@@ -10,7 +10,6 @@ class SkdevsecReviewViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = SkdevsecReview.objects.all()
     serializer_class = SkdevsecReviewSerializer
 
-    # sql 인젝션 되는 코드
     # 리뷰 출력
     @action(detail=False, methods=['POST'])
     def review_output(self, request):
@@ -24,46 +23,43 @@ class SkdevsecReviewViewSet(viewsets.ReadOnlyModelViewSet):
             pid = request.data['pid']
 
             # SQL 쿼리문 작성
-            strsql = "SELECT rid, rstar, unickname, rcreate_date FROM skdevsec_review where pid='" + pid + "' order by rid desc"
+            sql_query = "SELECT * FROM skdevsec_review where pid=%d order by rid desc"
 
             # DB에 명령문 전송
-            cursor.execute(strsql)
-            datas = cursor.fetchone()
+            cursor.execute(sql_query, (pid, ))
+            data = cursor.fetchone()
 
             # 데이터가 있으면
-            if datas is not None:
+            if data is not None:
                 # 데이터만큼 반복
-                while datas:
+                while data:
                     new_data_in = dict()
-                    new_data_in['rid'] = datas[0]
-                    new_data_in['rstar'] = datas[1]
-                    new_data_in['unickname'] = datas[2]
-                    new_data_in['rcreate_date'] = datas[3]
+                    new_data_in['rid'] = data[0]
+                    new_data_in['rstar'] = data[2]
+                    new_data_in['unickname'] = data[3]
+                    new_data_in['rcreate_date'] = data[4]
                     new_data.append(new_data_in)
-                    datas = cursor.fetchone()
+                    data = cursor.fetchone()
             # 데이터가 없으면
             else:
                 # DB와 접속 종료
-                connection.commit()
                 connection.close()
                 # 프론트엔드로 0 전송
                 return Response(0)
 
             # DB와 접속 종료
-            connection.commit()
             connection.close()
 
         # 에러가 발생했을 경우 백엔드에 에러 내용 출력 및 프론트엔드에 0 전송
         except Exception as e:
             connection.rollback()
-            print(f"review_output 에러: {e}")
+            print(f"에러: {e}")
             return Response(0)
 
         # 성공 했을 시, 프론트엔드에 데이터 전송
         else:
             return Response(new_data)
 
-    # sql 인젝션 되는 코드
     # 리뷰 작성
     @action(detail=False, methods=['POST'])
     def review_write(self, request):
@@ -78,16 +74,18 @@ class SkdevsecReviewViewSet(viewsets.ReadOnlyModelViewSet):
             rcreate_date = request.data['rcreate_date']
 
             # SQL 쿼리문 작성
-            strsql1 = "INSERT INTO skdevsec_review(pid, rstar, unickname, rcreate_date) VALUES('" + pid + "', '" + rstar + "', '" + unickname + "', '" + rcreate_date + "')"
+            sql_query_1 = "INSERT INTO skdevsec_review(pid, rstar, unickname, rcreate_date) VALUES(%d, %f, %s, %s)"
 
             # DB에 명령문 전송
-            cursor.execute(strsql1)
+            cursor.execute(sql_query_1, (pid, rstar, unickname, rcreate_date))
+            connection.commit()
 
             # SQL 쿼리문 작성
-            strsql2 = "UPDATE skdevsec_product SET preview = (SELECT COUNT(*) FROM skdevsec_review WHERE pid='" + pid + "'), preview_avg =(SELECT round(avg(rstar),1) FROM skdevsec_review WHERE pid='" + pid + "')  WHERE pid='" + pid + "'"
+            sql_query_2 = "UPDATE skdevsec_product SET preview = (SELECT COUNT(*) FROM skdevsec_review WHERE pid=%d), " \
+                          "preview_avg =(SELECT round(avg(rstar),1) FROM skdevsec_review WHERE pid=%d)  WHERE pid=%d "
 
             # DB에 명령문 전송
-            cursor.execute(strsql2)
+            cursor.execute(sql_query_2, (pid, pid, pid))
 
             # DB와 접속 종료
             connection.commit()
@@ -96,14 +94,13 @@ class SkdevsecReviewViewSet(viewsets.ReadOnlyModelViewSet):
         # 에러가 발생했을 경우 백엔드에 에러 내용 출력 및 프론트엔드에 0 전송
         except Exception as e:
             connection.rollback()
-            print(f"review_write 에러: {e}")
+            print(f"에러: {e}")
             return Response(0)
 
         # 성공 했을 시, 프론트 엔드에 1 전송
         else:
             return Response(1)
 
-    # sql 인젝션 되는 코드
     # 리뷰 삭제
     @action(detail=False, methods=['POST'])
     def comment_delete(self, request):
@@ -116,16 +113,18 @@ class SkdevsecReviewViewSet(viewsets.ReadOnlyModelViewSet):
             pid = request.data['pid']
 
             # SQL 쿼리문 작성
-            strsql = "DELETE FROM skdevsec_review WHERE rid='" + rid + "'"
+            sql_query_1 = "DELETE FROM skdevsec_review WHERE rid=%d"
 
             # DB에 명령문 전송
-            cursor.execute(strsql)
+            cursor.execute(sql_query_1, (rid, ))
+            connection.commit()
 
             # SQL 쿼리문 작성
-            strsql1 = "UPDATE skdevsec_product SET preview = (SELECT COUNT(*) FROM skdevsec_review WHERE pid='" + pid + "'), preview_avg =(SELECT round(avg(rstar),1) FROM skdevsec_review WHERE pid='" + pid + "')  WHERE pid='" + pid + "'"
+            sql_query_2 = "UPDATE skdevsec_product SET preview = (SELECT COUNT(*) FROM skdevsec_review WHERE pid=%s), " \
+                          "preview_avg =(SELECT round(avg(rstar),1) FROM skdevsec_review WHERE pid=%s)  WHERE pid=%s "
 
             # DB에 명령문 전송
-            cursor.execute(strsql1)
+            cursor.execute(sql_query_2, (pid, pid, pid,))
 
             # DB와 접속 종료
             connection.commit()
@@ -134,14 +133,13 @@ class SkdevsecReviewViewSet(viewsets.ReadOnlyModelViewSet):
         # 에러가 발생했을 경우 백엔드에 에러 내용 출력 및 프론트엔드에 0 전송
         except Exception as e:
             connection.rollback()
-            print(f"comment_delete 에러: {e}")
+            print(f"에러: {e}")
             return Response(0)
 
         # 성공 했을 시, 프론트엔드에 1 전송
         else:
             return Response(1)
 
-    # sql 인젝션 되는 코드
     # 리뷰 검증
     @action(detail=False, methods=['POST'])
     def review_certified(self, request):
@@ -151,37 +149,43 @@ class SkdevsecReviewViewSet(viewsets.ReadOnlyModelViewSet):
 
             # DB 접근할 cursor
             cursor = connection.cursor()
+            cursor_oid = connection.cursor()
 
             # POST 메소드로 날라온 Request의 데이터 각각 추출
             unickname = request.data['unickname']
             pname = request.data['pname']
 
             # SQL 쿼리문 작성
-            strsql = "SELECT uid FROM skdevsec_user WHERE unickname='" + unickname + "'"
+            sql_query_1 = "SELECT * FROM skdevsec_user WHERE unickname=%s"
 
             # DB에 명령문 전송
-            cursor.execute(strsql)
+            cursor.execute(sql_query_1, (unickname, ))
             uid = cursor.fetchone()
 
-            # SQL 쿼리문 작성
-            strsql1 = "SELECT oid FROM skdevsec_orderuser WHERE uid='" + uid[0] + "'"
-
-            # DB에 명령문 전송
-            cursor.execute(strsql1)
-            oids = cursor.fetchall()
-
-            for oid in oids:
+            if uid is not None:
                 # SQL 쿼리문 작성
-                strsql2 = "SELECT pname FROM skdevsec_orderproduct WHERE oid='" + str(oid[0]) + "'"
+                sql_query_2 = "SELECT * FROM skdevsec_orderuser WHERE uid=%d"
 
                 # DB에 명령문 전송
-                cursor.execute(strsql2)
-                pnames = cursor.fetchall()
+                cursor_oid.execute(sql_query_2, (uid[0], ))
+                oid = cursor_oid.fetchone()
 
-                for _pname in pnames:
-                    pname_list.append(_pname[0])
+            else:
+                return Response(0)
+
+            while oid:
+                # SQL 쿼리문 작성
+                sql_query_3 = "SELECT * FROM skdevsec_orderproduct WHERE oid=%d"
+
+                # DB에 명령문 전송
+                cursor.execute(sql_query_3, (oid[0], ))
+                _pname = cursor.fetchone()
+
+                pname_list.append(_pname[2])
+                oid = cursor_oid.fetchone()
 
             pname_count = {}
+
             for i in pname_list:
                 try:
                     pname_count[i] += 1
@@ -191,35 +195,34 @@ class SkdevsecReviewViewSet(viewsets.ReadOnlyModelViewSet):
             # 만약 사용자가 상품을 구매했으면 리뷰한 적이 있는지 확인
             if pname in pname_list:
                 # SQL 쿼리문 작성
-                strsql3 = "SELECT pid FROM skdevsec_product WHERE pname='" + pname + "'"
+                sql_query_4 = "SELECT * FROM skdevsec_product WHERE pname=%s"
 
                 # DB에 명령문 전송
-                cursor.execute(strsql3)
+                cursor.execute(sql_query_4, (pname, ))
                 pid = cursor.fetchone()
 
                 # SQL 쿼리문 작성
-                strsql4 = "SELECT COUNT(*) FROM skdevsec_review WHERE pid='" + str(pid[0]) + "' AND unickname='" + unickname + "'"
+                sql_query_5 = "SELECT COUNT(*) FROM skdevsec_review WHERE pid=%d AND unickname=%s"
 
                 # DB에 명령문 전송
-                cursor.execute(strsql4)
-                review = cursor.fetchall()
+                cursor.execute(sql_query_5, (pid[0], unickname, ))
+                review = cursor.fetchone()
             # 구매한적이 없으면 프론트엔드로 0 전송
             else:
                 return Response(0)
 
             # DB와 접속 종료
-            connection.commit()
             connection.close()
 
         # 에러가 발생했을 경우 백엔드에 에러 내용 출력 및 프론트엔드에 0 전송
         except Exception as e:
             connection.rollback()
-            print(f"review_certified 에러: {e}")
+            print(f"에러: {e}")
             return Response(0)
 
         # 리뷰를 작성할 수 없으면 프론트엔드에 1 전송, 작성할 수 있으면 0 전송
         else:
-            if pname_count[pname] <= int(review[0][0]):
+            if pname_count[pname] <= int(review[0]):
                 return Response(1)
             else:
                 return Response(0)
