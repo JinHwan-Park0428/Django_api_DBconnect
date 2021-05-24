7181dfd11f2df0407ebd3e885bdf0b50e2b59de9
JinHwan-Park0428 <zkzlaptb6@gmail.com>
2021-05-24 10:03:50 +0900

    settings 및 urls 파일 보안 수정

--- DBtest/settings.py
+++ DBtest/settings.py
@@ -1,27 +1,28 @@
 # 모듈 임포트
 import os
 from pathlib import Path
+import mysettings
 
 
 # 베이스 디렉토리 경로 (ex : C:\Users\soopeng\PycharmProjects)
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Django Secure Key
-SECRET_KEY = 'django-insecure-z9=1t9nxitfv6sj!((ta7xn!u+6ixa$rmmzib-xbmh7_#qi&#f'
+SECRET_KEY = mysettings.SECRET_KEY
 
 # 이메일 보내기 위한 기본 세팅
-EMAIL_HOST = 'smtp.naver.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'zkzlaptb@naver.com'
-EMAIL_HOST_PASSWORD = 'Pjh0428!'
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
+EMAIL_HOST = mysettings.EMAIL_HOST
+EMAIL_PORT = mysettings.EMAIL_PORT
+EMAIL_HOST_USER = mysettings.EMAIL_HOST_USER
+EMAIL_HOST_PASSWORD = mysettings.EMAIL_HOST_PASSWORD
+EMAIL_USE_TLS = mysettings.EMAIL_USE_TLS
+EMAIL_USE_SSL = mysettings.EMAIL_USE_SSL
 
 # Debug 허용/비허용 (나중에 False 해야함)
 DEBUG = True
 
 # 접속 가능한 호스트('*' <== 전부 허용)
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost']
 
 # 앱
 INSTALLED_APPS = [
@@ -50,6 +51,8 @@ MIDDLEWARE = [
 
 # 외부 웹사이트 접근 허용 여부
 CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_WHITELIST = ['http://www.kilhyomin.com/', ]
+CORS_ALLOW_CREDENTIALS = True
 
 # 아직까지 건들 일 없음
 ROOT_URLCONF = 'DBtest.urls'
@@ -75,19 +78,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'DBtest.wsgi.application'
 
 # MYSQL 데이터베이스 설정
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'yeonsiwoo3',
-        'USER': 'yeonsiwoo3',
-        'PASSWORD': 'Hyomin2332@',
-        'HOST': 'nodejs-009.cafe24.com',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        }
-    }
-}
+DATABASES = mysettings.DATABASES
 
 # 아직까지 건들 일 없음
 AUTH_PASSWORD_VALIDATORS = [
