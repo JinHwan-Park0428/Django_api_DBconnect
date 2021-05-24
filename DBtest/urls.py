eaae669b22d1349b2e02900e94420ef957668f0b
JinHwan-Park0428 <zkzlaptb6@gmail.com>
2021-05-24 10:16:12 +0900

    settings 및 urls 파일 보안 수정

--- DBtest/urls.py
+++ DBtest/urls.py
@@ -1,6 +1,5 @@
 # 필요한 모듈 임포트
-from django.contrib import admin
 from django.urls import include, path
 
 # 접근 가능한 url 패턴 목록
-urlpatterns = path('', include('DBConnect.urls'))
+urlpatterns = [path('', include('DBConnect.urls')), ]
