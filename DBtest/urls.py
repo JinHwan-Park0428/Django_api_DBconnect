5ce00b358244249a4e0b4432b6d31f786fa630c2
JinHwan-Park0428 <zkzlaptb6@gmail.com>
2021-05-24 09:44:00 +0900

    settings 및 urls 파일 보안 수정

--- DBtest/urls.py
+++ DBtest/urls.py
@@ -3,8 +3,4 @@ from django.contrib import admin
 from django.urls import include, path
 
 # 접근 가능한 url 패턴 목록
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('DBConnect.urls')),
-    # path('apt-auth', include('rest_framework.urls', namespace='rest_framework'))
-]
+urlpatterns = path('', include('DBConnect.urls'))
