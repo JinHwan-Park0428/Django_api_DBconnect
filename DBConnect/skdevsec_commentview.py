8f4e4797fa72695144c081e163e85bb152e5bc04
JinHwan-Park0428 <zkzlaptb6@gmail.com>
2021-05-24 15:35:08 +0900

    skdevsec_commentview 함수 SQL Injection 대비

--- DBConnect/skdevsec_commentview.py
+++ DBConnect/skdevsec_commentview.py
@@ -11,7 +11,6 @@ class SkdevsecCommentViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = SkdevsecComment.objects.all()
     serializer_class = SkdevsecCommentSerializer
 
-    # sql 인젝션 되는 코드
     # 댓글 출력
     @action(detail=False, methods=['POST'])
     def view_comment(self, request):
@@ -24,13 +23,12 @@ class SkdevsecCommentViewSet(viewsets.ReadOnlyModelViewSet):
             # POST 메소드로 날라온 Request의 데이터 각각 추출
             bid = request.data['bid']
             cpage = request.data['cpage']
-            cpage = int(cpage)
 
             # SQL 쿼리문 작성
-            strsql = "SELECT COUNT(*) FROM skdevsec_comment WHERE bid='" + bid + "'"
+            sql_query_1 = "SELECT COUNT(*) FROM skdevsec_comment WHERE bid=%d"
 
             # DB에 명령문 전송
-            cursor.execute(strsql)
+            cursor.execute(sql_query_1, (bid, ))
             count = cursor.fetchone()
 
             if count is not None:
@@ -38,43 +36,40 @@ class SkdevsecCommentViewSet(viewsets.ReadOnlyModelViewSet):
                 new_data.append({"comment_count": count[0]})
 
                 # SQL 쿼리문 작성
-                strsql1 = "SELECT cid, unickname, ctext, ccreate_date, clock FROM skdevsec_comment where bid='" + bid + "' order by cid LIMIT " + str(
-                    cpage * 10 - 10) + " , 10"
+                sql_query_2 = "SELECT * FROM skdevsec_comment where bid=%d order by cid LIMIT %d, 10"
 
                 # DB에 명령문 전송
-                cursor.execute(strsql1)
-                datas = cursor.fetchone()
+                cursor.execute(sql_query_2, (bid, cpage))
+                data = cursor.fetchone()
 
                 # 데이터 갯수만큼 반복
-                while datas:
+                while data:
                     new_data_in = dict()
-                    new_data_in['cid'] = datas[0]
-                    new_data_in['unickname'] = datas[1]
-                    new_data_in['ctext'] = datas[2]
-                    new_data_in['ccreate_date'] = datas[3]
-                    new_data_in['clock'] = datas[4]
+                    new_data_in['cid'] = data[0]
+                    new_data_in['unickname'] = data[2]
+                    new_data_in['ctext'] = data[3]
+                    new_data_in['ccreate_date'] = data[4]
+                    new_data_in['clock'] = data[5]
                     new_data.append(new_data_in)
-                    datas = cursor.fetchone()
+                    data = cursor.fetchone()
 
             else:
                 connection.close()
                 return Response({"comment_count": 0})
 
             # DB와 접속 종료
-            connection.commit()
             connection.close()
 
         # 에러가 발생했을 경우 백엔드에 에러 내용 출력 프론트엔드에 0 전송
         except Exception as e:
             connection.rollback()
-            print(f"view_comment 에러: {e}")
+            print(f"에러: {e}")
             return Response(0)
 
         # 성공 했을 시, 프론트엔드에 데이터 전송
         else:
             return Response(new_data)
 
-    # sql 인젝션 되는 코드
     # 댓글 작성
     @action(detail=False, methods=['POST'])
     def comment_write(self, request):
@@ -90,16 +85,17 @@ class SkdevsecCommentViewSet(viewsets.ReadOnlyModelViewSet):
             clock = str(request.data['clock'])
 
             # SQL 쿼리문 작성
-            strsql = "INSERT INTO skdevsec_comment(bid, unickname, ctext, ccreate_date, clock) VALUES('" + bid + "', '" + unickname + "', '" + ctext + "', '" + ccreate_date + "', '" + clock + "')"
+            sql_query_1 = "INSERT INTO skdevsec_comment(bid, unickname, ctext, ccreate_date, clock) VALUES(%d, %s, %s, %s, %s)"
 
             # DB에 명령문 전송
-            cursor.execute(strsql)
+            cursor.execute(sql_query_1, (bid, unickname, ctext, ccreate_date, clock, ))
+            connection.commit()
 
             # SQL 쿼리문 작성
-            strsql1 = "UPDATE skdevsec_board SET bcomment=bcomment+1 WHERE bid='" + bid + "'"
+            sql_query_2 = "UPDATE skdevsec_board SET bcomment=bcomment+1 WHERE bid=%d"
 
             # DB에 명령문 전송
-            cursor.execute(strsql1)
+            cursor.execute(sql_query_2, (bid, ))
 
             # 데이터를 사용완료 했으면 DB와 접속 종료(부하 방지)
             connection.commit()
@@ -108,7 +104,7 @@ class SkdevsecCommentViewSet(viewsets.ReadOnlyModelViewSet):
         # 에러가 발생했을 경우 백엔드에 에러 내용 출력 및 프론트엔드에 0 전송
         except Exception as e:
             connection.rollback()
-            print(f"comment_write 에러: {e}")
+            print(f"에러: {e}")
             return Response(0)
 
         # 성공 했을 시, 프론트엔드에 1 전송
@@ -128,16 +124,17 @@ class SkdevsecCommentViewSet(viewsets.ReadOnlyModelViewSet):
             cid = request.data['cid']
 
             # SQL 쿼리문 작성
-            strsql = "DELETE FROM skdevsec_comment WHERE cid='" + cid + "'"
+            sql_query_1 = "DELETE FROM skdevsec_comment WHERE cid=%d"
 
             # DB에 명령문 전송
-            cursor.execute(strsql)
+            cursor.execute(sql_query_1, (cid, ))
+            connection.commit()
 
             # SQL 쿼리문 작성
-            strsql1 = "UPDATE skdevsec_board SET bcomment=bcomment-1 WHERE bid='" + bid + "'"
+            sql_query_2 = "UPDATE skdevsec_board SET bcomment=bcomment-1 WHERE bid=%d"
 
             # DB에 명령문 전송
-            cursor.execute(strsql1)
+            cursor.execute(sql_query_2, (bid, ))
 
             # 데이터를 사용완료 했으면 DB와 접속 종료(부하 방지)
             connection.commit()
@@ -146,7 +143,7 @@ class SkdevsecCommentViewSet(viewsets.ReadOnlyModelViewSet):
         # 에러가 발생했을 경우 백엔드에 에러 내용 출력 및 프론트엔드 0 전송
         except Exception as e:
             connection.rollback()
-            print(f"comment_delete 에러: {e}")
+            print(f"에러: {e}")
             return Response(0)
 
         # 성공 했을 시, 프론트엔드에 1 전송
