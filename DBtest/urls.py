c33562269faa59e1e22a4cfb6adf3e1fb7c3ff66
JinHwan-Park0428 <zkzlaptb6@gmail.com>
2021-05-26 09:35:31 +0900

    Static 파일 자동화 실험

--- DBtest/urls.py
+++ DBtest/urls.py
@@ -4,5 +4,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 # 접근 가능한 url 패턴 목록
-urlpatterns = [path('', include('DBConnect.urls')), ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns = [path('', include('DBConnect.urls')), ]
+
+# + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
