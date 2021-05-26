eb3fd69f4713706e1f07713803c4444c6d22b704
JinHwan-Park0428 <zkzlaptb6@gmail.com>
2021-05-26 14:25:47 +0900

    암호화

---
+++ DBConnect/security.py
@@ -0,0 +1,23 @@
+from Crypto import Random
+from Crypto.Cipher import AES
+import base64
+
+BS = 16
+pad = lambda s: s + (BS - len(s.encode('utf-8')) % BS) * chr(BS - len(s.encode('utf-8')) % BS)
+unpad = lambda s : s[:-ord(s[len(s)-1:])]
+
+class AESCipher:
+    def __init__( self, key ):
+        self.key = key
+
+    def encrypt( self, raw ):
+        raw = pad(raw)
+        iv = Random.new().read( AES.block_size )
+        cipher = AES.new( self.key, AES.MODE_CBC, iv )
+        return base64.b64encode( iv + cipher.encrypt( raw.encode('utf-8') ) )
+
+    def decrypt( self, enc ):
+        enc = base64.b64decode(enc)
+        iv = enc[:16]
+        cipher = AES.new(self.key, AES.MODE_CBC, iv )
+        return unpad(cipher.decrypt( enc[16:] ))
