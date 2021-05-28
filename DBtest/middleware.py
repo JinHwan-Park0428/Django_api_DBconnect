7357645084dbbd2227f0185e1e4a5a92a9664a75
JinHwan-Park0428 <zkzlaptb6@gmail.com>
2021-05-28 16:15:25 +0900

    settings 수정

--- DBtest/middleware.py
+++ DBtest/middleware.py
@@ -1,4 +1,9 @@
-class RemoveHeaders(object):
-    def process_response(self, request, response):
-        del response['Server']
+class SimpleMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        # One-time configuration and initialization.
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        response.__setitem__('Server', '')
         return response
