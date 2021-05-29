524ae1e01ab2bebbb4ae7ebea12d3c8a8963f880
JinHwan-Park0428 <zkzlaptb6@gmail.com>
2021-05-29 16:48:47 +0900

    skdevsec_userview.py 수정

--- DBConnect/security.py
+++ DBConnect/security.py
@@ -4,7 +4,7 @@ import hashlib
 from Crypto import Random
 from Crypto.Cipher import AES
 
-from Crypto.Util.Padding import pad
+# from Crypto.Util.Padding import pad
 
 BS = 32
 pad = lambda s: s + (BS - len(s.encode('utf-8')) % BS) * chr(BS - len(s.encode('utf-8')) % BS)
@@ -18,7 +18,8 @@ class AESCipher:
     def encrypt(self, raw):
         raw = pad(raw)
         # iv = Random.new().read(AES.block_size)
-        iv = 1234567812345678
+        iv = '1234567812345678'
+        iv = base64.b64encode(iv)
         # iv = iv.encode('utf-8')
         cipher = AES.new(self.key, AES.MODE_CBC, iv)
         return base64.b64encode(iv + cipher.encrypt(raw.encode('utf-8')))
@@ -26,7 +27,8 @@ class AESCipher:
     def decrypt(self, enc):
         enc = base64.b64decode(enc)
         # iv = enc[:16]
-        iv = 1234567812345678
+        iv = '1234567812345678'
+        iv = base64.b64encode(iv)
         # iv = iv.encode('utf-8')
         cipher = AES.new(self.key, AES.MODE_CBC, iv)
         return unpad(cipher.decrypt(enc[16:]))
@@ -56,34 +58,34 @@ class AESCipher:
 #     @staticmethod
 #     def _unpad(s):
 #         return s[:-ord(s[len(s) - 1:])]
-
-class AesCrypt:
-    def __init__(self, model, iv, encode_, key):
-        self.encode_ = encode_
-        self.model = {'ECB': AES.MODE_ECB, 'CBC': AES.MODE_CBC}[model]
-        self.key = self.add_16(key)
-        if model == 'ECB':
-            self.aes = AES.new(self.key, self.model)  # Create an aes object
-        elif model == 'CBC':
-            self.aes = AES.new(self.key, self.model, iv)  # Create an aes object
-
-        # Here the key length must be 16, 24 or 32, and the current 16-bit is enough.
-
-    def add_16(self, par):
-        par = par.encode(self.encode_)
-        while len(par) % 16 != 0:
-            par += b'\x00'
-        return par
-
-    # encryption
-    def aesencrypt(self, text):
-        text = pad(text.encode('utf-8'), AES.block_size, style='pkcs7')
-        self.encrypt_text = self.aes.encrypt(text)
-        return base64.encodebytes(self.encrypt_text).decode().strip()
-
-    # Decrypt
-    def aesdecrypt(self, text):
-        text = base64.decodebytes(text.encode(self.encode_))
-        self.decrypt_text = self.aes.decrypt(text)
-        return self.decrypt_text.decode(self.encode_).strip('\0').strip("\n")
+#
+# class AesCrypt:
+#     def __init__(self, model, iv, encode_, key):
+#         self.encode_ = encode_
+#         self.model = {'ECB': AES.MODE_ECB, 'CBC': AES.MODE_CBC}[model]
+#         self.key = self.add_16(key)
+#         if model == 'ECB':
+#             self.aes = AES.new(self.key, self.model)  # Create an aes object
+#         elif model == 'CBC':
+#             self.aes = AES.new(self.key, self.model, iv)  # Create an aes object
+#
+#         # Here the key length must be 16, 24 or 32, and the current 16-bit is enough.
+#
+#     def add_16(self, par):
+#         par = par.encode(self.encode_)
+#         while len(par) % 16 != 0:
+#             par += b'\x00'
+#         return par
+#
+#     # encryption
+#     def aesencrypt(self, text):
+#         text = pad(text.encode('utf-8'), AES.block_size, style='pkcs7')
+#         self.encrypt_text = self.aes.encrypt(text)
+#         return base64.encodebytes(self.encrypt_text).decode().strip()
+#
+#     # Decrypt
+#     def aesdecrypt(self, text):
+#         text = base64.decodebytes(text.encode(self.encode_))
+#         self.decrypt_text = self.aes.decrypt(text)
+#         return self.decrypt_text.decode(self.encode_).strip('\0').strip("\n")
 
